715f179317ea05e17a2a97b8bf5d195b6d1c1e0b
Panda1246 <samek.jakub02@gmail.com>
2025-01-08 22:28:54 +0100

    First part of task complete

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,  abort
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///snails.db'
 db = SQLAlchemy(app)
-atributes = ['sex', 'length', 'diameter', 'height', 'whole_weight', 'shucked_weight', 'viscera_weight', 'shell_weight',
-             'rings']
+atributes = ['sex', 'length', 'diameter', 'rings']
 
 
 class Abalone(db.Model):
@@ -13,11 +12,6 @@ class Abalone(db.Model):
     sex = db.Column(db.String(1))
     length = db.Column(db.Float)
     diameter = db.Column(db.Float)
-    height = db.Column(db.Float)
-    whole_weight = db.Column(db.Float)
-    shucked_weight = db.Column(db.Float)
-    viscera_weight = db.Column(db.Float)
-    shell_weight = db.Column(db.Float)
     rings = db.Column(db.Integer)
 
 
@@ -36,22 +30,20 @@ def add():
     if request.method == 'POST':
         for i in atributes:  # value veryfication
             if i not in request.form:
-                pass  # return error missing value
+                abort(400)
             value_of_arg = request.form[i]
             if i == 'sex':
                 if value_of_arg not in ['M', 'F', 'I']:
                     pass  # return error wrong value
             elif i == 'rings':
                 if not value_of_arg.isdigit():
-                    pass
+                    abort(400)
             else:
                 try:
                     float(value_of_arg)
                 except ValueError:
-                    pass
-        abalone = Abalone(sex=request.form['sex'], length=request.form['length'], diameter=request.form['diameter'], height=request.form['height'],
-                          whole_weight=request.form['whole_weight'], shucked_weight=request.form['shucked_weight'], viscera_weight=request.form['viscera_weight'],
-                          shell_weight=request.form['shell_weight'], rings=request.form['rings'])
+                    abort(400)
+        abalone = Abalone(sex=request.form['sex'], length=request.form['length'], diameter=request.form['diameter'], rings=request.form['rings'])
         try:
             db.session.add(abalone)
             db.session.commit()
@@ -63,12 +55,21 @@ def add():
 @app.route('/delete/<int:id>')
 def delete(id):
     try:
-        db.session.delete(Abalone.query.get_or_404(id))
+        db.session.delete(Abalone.query.get(id))
         db.session.commit()
         return redirect('/')
     except Exception as e:
-        return f"Database connection failed: {str(e)}"  # return error
+        return abort(404)  # return error
 
+@app.errorhandler(400)
+def bad_request(error):
+    return render_template('400.jinja'), 400
+@app.errorhandler(404)
+def bad_request(error):
+    return render_template('404.jinja'), 404
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.jinja'), 500
 
 if __name__ == '__main__':
     app.run()
